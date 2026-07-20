b3a51922ba6f16b549f6c0ad65d9575f3e9e41ba
Debar Flavien <flavien.debar@gmail.com>
2026-07-20 16:44:37 +0200

    feat: add config for logging

--- config.py
+++ config.py
@@ -1,10 +1,27 @@
 from dataclasses import dataclass
+import logging
 import os
 
 
 @dataclass(slots=True)
 class Settings:
-    log_tail_lines: int = int(os.getenv("LOG_TAIL_LINES", "200"))
+
+    log_tail_lines: int = int(
+        os.getenv("LOG_TAIL_LINES", "200")
+    )
+
+    log_level: str = os.getenv(
+        "LOG_LEVEL",
+        "INFO"
+    )
 
 
 settings = Settings()
+
+
+logging.basicConfig(
+    level=getattr(logging, settings.log_level),
+    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
+)
+
+logger = logging.getLogger("kubesage")
