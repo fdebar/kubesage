16d5847c8960d6e44ccd533ee9adb3f111095c6f
Debar Flavien <flavien.debar@gmail.com>
2026-07-29 17:35:33 +0200

    feat: does not include schema in docs

--- kubesage/api/routers/metrics.py
+++ kubesage/api/routers/metrics.py
@@ -4,6 +4,6 @@ from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 router = APIRouter(tags=["Metrics"])
 
 
-@router.get("/metrics", include_in_schema=True)
+@router.get("/metrics", include_in_schema=False)
 def metrics() -> Response:
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
